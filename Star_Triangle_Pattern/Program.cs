5a91c7d76626ece2c462ce04f6aada4da542cb36
Vignesh P <vicky16987@gmail.com>
2022-09-12 20:55:50 +0530

    Star Triangle Pattern Project Added

---
+++ Star_Triangle_Pattern/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Star_Triangle_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRows = 0;
+            int space = 0;
+            int length = 1;
+            Console.WriteLine("Enter the number of Rows: ");
+            numberOfRows = Convert.ToInt32(Console.ReadLine());
+            space = numberOfRows - 1;
+
+            for(int i = 1; i <= numberOfRows; i++)
+            {
+                for(int s = 1; s <= space; s++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int l = 1; l <= length; l++)
+                {
+                    Console.Write("*");
+                }
+
+                space--;
+                length = length + 2;
+                Console.WriteLine();
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
